c84b9239f195fab69f8b60758f8089f0456b417f
Frederick Ross <ross@ptbbpc1.epfl.ch>
2010-12-30 15:41:40 +0100

    Wrote docstring for __init__.py

--- bbcflib/__init__.py
+++ bbcflib/__init__.py
@@ -1,4 +1,32 @@
 """
+===========
+bbcflib 0.1
+===========
+
+bbcflib is a set of Python modules for accessing facilities used by
+the Bioinformatics and Biostatistics Core Facility (BBCF) at the EPFL.
+At the moment it provides modules for BBCF's GenRep, the LIMS of the
+DNA Array Facility at UNIL, the standard frontend code for workflows
+written by Fabrice David, and sending job description emails.
+
+All the functionality can be imported with::
+
+    import bbcflib
+
+which reexports the important functions from all the submodules.  It
+reexports ``ConfigParser`` from ``ConfigParser``, and the following
+modules from bbcflib:
+
+.. autoclass:: GenRep
+
+.. autoclass:: Assembly
+
+.. autoclass:: EmailReport
+
+.. autoclass:: DAFLIMS
+
+.. autoclass:: Frontend
+
 """
 
 from ConfigParser import ConfigParser
