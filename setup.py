ba6315bea371ae3a3ca15ad681c25f168469b6ef
Frederick Ross <ross@ptbbpc1.epfl.ch>
2010-12-30 11:38:17 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bbcflib',
+      version='0.1',
+      description='Utility modules for deploying workflows in the BBCF',
+      author='Fred Ross',
+      author_email='madhadron@gmail.com',
+      packages=['bbcflib'],
+      classifiers=['Topic :: Scientific/Engineering :: Bio-Informatics']
+      )
