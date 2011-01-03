94bbb4f564c79ac4e059cd010fbf34516b1085c1
Frederick Ross <ross@ptbbpc1.epfl.ch>
2011-01-03 14:35:56 +0100

    Wrote __init__ for DAFLIMS

--- bbcflib/daflims.py
+++ bbcflib/daflims.py
@@ -2,4 +2,15 @@
 """
 
 class DAFLIMS(object):
-    pass
+    def __init__(self, username=None, password=None, config=None, section="daflims"):
+        if (username==None or password==None) and config==None:
+            raise TypeError("Must provide a username and password, or a ConfigParser.")
+        elif config != None:
+            if username == None: self.username = config.get(section, 'daflims_username')
+            else:                self.username = username
+
+            if password == None: self.password = config.get(section, 'daflims_password')
+            else:                self.password = password
+        else:
+            self.username = username
+            self.password = password
