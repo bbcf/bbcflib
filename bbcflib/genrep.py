909b721d72449c6fbd5ca91bd8bd0018bc152d20
Jacques Rougemont <rougemon@ptbbpc1.epfl.ch>
2011-03-18 11:11:04 +0100

    adding fasta_from_bed

--- bbcflib/genrep.py
+++ bbcflib/genrep.py
@@ -96,6 +96,85 @@ class GenRep(object):
             raise ValueError("Argument 'assembly' to must be a " + \
                                  "string or integer, got " + str(assembly))
 
+    def get_sequence(self, chr_id, coord_list):
+        """Parses a slice request to the repository."""
+        if len(coord_list) == 0:
+            return []
+        slices = ",".join([",".join([str(y) for y in x]) for x in coord_list])
+        url = """%s/chromosomes/%i/get_sequence_part?slices=%s""" % (self.url, chr_id, slices)
+        return urllib2.urlopen(url).read().split(',')
+
+    def fasta_from_bed(self, chromosomes, out=None, bed=None, sql=None, chunk=100000):
+        """Get a fasta file with sequences corresponding to the features in the 
+        bed or sqlite file.
+
+        Returns the name of the file and the total sequence size.
+        """
+        def push_slices(slices,start,end,name,cur_chunk):
+            slices['coord'].append([start,end])
+            slices['names'].append(name)
+            cur_chunk += e-s
+            return slices,cur_chunk
+        def flush_slices(slices,cid,out):
+            names=slices['names']
+            coord=slices['coord']
+            chr = chr_names[cid]
+            with open(out,"a") as f:
+                for i,s in enumerate(self.get_sequence(cid,coord)):
+                    f.write(">"+names[i]+" "+chr+":"+str(coord[i][0])+"-"+str(coord[i][1])+"\n"+s+"\n")
+            return {'coord':[],'names':[]}
+        slices = {'coord':[],'names':[]}
+        chr_names = dict((c[0],cn['name']) for c,cn in chromosomes.iteritems())
+        chr_len = dict((c[0],cn['length']) for c,cn in chromosomes.iteritems())
+        size = 0
+        if bed != None:
+            if out == None:
+                out = bed+".fa"
+            chr_ids = dict((cn['name'],c[0]) for c,cn in chromosomes.iteritems())
+            cur_chunk = 0
+            cur_chr = 0
+            with open(bed,"r") as f:
+                for l in f:
+                    row = l.split("\t")
+                    cid = chr_ids[row[0]]
+                    s = max(int(row[1]),0)
+                    e = min(int(row[2]),chr_len[cid])
+                    name = ''
+                    if len(row)>3:
+                        name = row[3]
+                    slices,cur_chunk = push_slices(slices,s,e,name,cur_chunk)
+                    if (cur_chr>0 and cur_chr != cid) or cur_chunk > chunk:
+                        size += cur_chunk
+                        slices = flush_slices(slices,cur_chr,out)
+                        cur_chunk = 0
+                    cur_chr = cid
+                size += cur_chunk
+                slices = flush_slices(slices,cur_chr,out)
+        elif sql != None:
+            if out == None:
+                out = sql+".fa"
+            cur_chunk = 0
+            connection = sqlite3.connect( sql )
+            cur = connection.cursor()
+            for k in chromosomes.keys():
+                cur.execute('select start,end,name from "'+chr_names[k]+'"')
+                connection.commit()
+                for row in cur:
+                    s = max(row[0],0)
+                    e = min(row[1],chr_len[cid])
+                    name = row[2]
+                    slices,cur_chunk = push_slices(slices,s,e,name,cur_chunk)
+                    if cur_chunk > chunk:
+                        size += cur_chunk
+                        slices = flush_slices(slices,k,f)
+                        cur_chunk = 0
+                    size += cur_chunk
+                    slices = flush_slices(slices,k,f)
+                cur.close()
+        else:
+            raise TypeError("fasta_from_bed requires either a 'sqlite' or a 'bed' file.")
+        return (out,size)
+
     def assembly(self, assembly):
         """Get an Assembly object corresponding to *assembly*.
 
@@ -209,9 +288,9 @@ class Assembly(object):
             {'name': name, 'length': length}
 
         
-        
-        
 
+
+        
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
