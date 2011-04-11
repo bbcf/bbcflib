b5c68def5014d0da4fc629663d71618c81376991
Jacques Rougemont <jacques.rougemont@epfl.ch>
2011-04-11 13:48:23 +0200

    remove redundant code, check urls

--- bbcflib/gdv.py
+++ bbcflib/gdv.py
@@ -7,6 +7,7 @@ Python API for the GDV genome viewer.
 """
 import urllib, urllib2
 import json
+from bbcflib.common import normalize_url
 ############ GDV requests ############
                                                                                                                                   
 def create_gdv_project( gdv_key, gdv_email,
@@ -29,7 +30,8 @@ def create_gdv_project( gdv_key, gdv_email,
                 "name": str(name),
                 "seq_id": str(nr_assembly_id),
                 "public": str(public).lower() }
-    return json.load(urllib2.urlopen( gdv_url+"/post", urllib.urlencode(request))) 
+    gdv_url = normalize_url(gdv_url)+"/post"
+    return json.load(urllib2.urlopen( gdv_url, urllib.urlencode(request))) 
 
 def get_project_id(json):
     return json['project_id']
@@ -40,7 +42,9 @@ def add_gdv_track( gdv_key, gdv_email,
                    project_id,
                    url,
                    name=None,
-                   gdv_url="http://svitsrv25.epfl.ch/gdv" ):
+                   gdv_url="http://svitsrv25.epfl.ch/gdv",
+                   datatype="quantitative",
+                   sqlite = False ):
     '''
     Add a new track on a project on GDV
     :param gdv_email: your login in TEQUILA
@@ -54,10 +58,14 @@ def add_gdv_track( gdv_key, gdv_email,
                 "key": gdv_key,
                 "command": "add_track",
                 "project_id": str(project_id),
-                "url": str(url) }
+                "url": normalize_url(str(url)) }
+    if sqlite:
+        request["command"] = "add_sqlite"
+        request["datatype"] = datatype
     if name != None: 
         request['name']=name
-    return urllib2.urlopen( gdv_url+"/post", urllib.urlencode(request) ).read()
+    gdv_url = normalize_url(gdv_url)+"/post"
+    return urllib2.urlopen( gdv_url, urllib.urlencode(request) ).read()
     
 def add_gdv_sqlite( gdv_key, gdv_email,
                     project_id,
@@ -65,35 +73,19 @@ def add_gdv_sqlite( gdv_key, gdv_email,
                     name=None,
                     gdv_url="http://svitsrv25.epfl.ch/gdv",
                     datatype="quantitative" ):
-    '''
-    Add a new track on a project on GDV
-    :param gdv_email: your login in TEQUILA
-    :param gdv_key: your user key (get it from your GDV account)
-    :param name: name of the track -optionnal- (will take the file name by default)
-    :param project_id: the project id to add the track
-    :param url : the URL where to fetch the file
-    '''
-    request = { "id": "gdv_post",
-                "mail": gdv_email,
-                "key": gdv_key,
-                "command": "add_sqlite",
-                "project_id": str(project_id),
-                "url": str(url),
-                "datatype": datatype }
-    if name != None: 
-        request['name'] = name
-    return urllib2.urlopen( gdv_url+"/post", urllib.urlencode(request)).read()
+    return add_gdv_track(gdv_key,gdv_email,project_id,url,name,gdv_url,datatype,True)
 
 
 def add_sql_files( gdv_key, gdv_email,
                    project_id,
                    files, names,
-                   serv_url="http://htsstation.vital-it.ch/chipseq",
+                   serv_url="http://htsstation.vital-it.ch/lims/chipseq/chipseq_minilims.files",
                    gdv_url="http://svitsrv25.epfl.ch/gdv",
                    datatype="quantitative" ):
-    return [add_gdv_sqlite(gdv_key,gdv_email,project_id,
-                           serv_url+"/get_file?name="+f, names[i],
-                           gdv_url,dataype) 
+    serv_url = normalize_url(serv_url)
+    return [add_gdv_track( gdv_key, gdv_email, project_id,
+                           serv_url+"/"+f, names[i],
+                           gdv_url, dataype, True ) 
             for i,f in enumerate(files)]
 
 ############################################################     
