f0e77e5f42e428daaa34d9c4cb6eba581e7a10ab
Lucas Sinclair <lucas.sinclair@me.com>
2011-04-20 15:17:56 +0200

    Fixed setup.py (missing commas)

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setup(
       name             = 'bbcflib',
       version          = '1.1',
       description      = 'Utility modules for deploying workflows in the BBCF',
-      long_description = open('README.md').read()
-      license          = open('LICENSE.txt').read(),
+      long_description = open('README.md').read(),
+      license          = 'GNU General Public License 3.0',
       url              = 'http://bbcf.epfl.ch/bbcflib',
       author           = 'EPFL BBCF, Fred Ross, Jacques Rougemont, Lucas Sinclair, Jonathan Mercier.',
       author_email     = 'webmaster.bbcf@epfl.ch',
       install_requires = ['mailer', 'bein'],
-      classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics']
-      packages         = ['bbcflib'
-                          'bbcflib.tests'
-                          'bbcflib.track'
+      classifiers      = ['Topic :: Scientific/Engineering :: Bio-Informatics'],
+      packages         = ['bbcflib',
+                          'bbcflib.tests',
+                          'bbcflib.track',
                          ],
 )
