4525f149a3b766ed5860be2baabf99960ae5c48a
Lucas Sinclair <lucas.sinclair@me.com>
2011-04-26 18:07:15 +0200

    Added a usefull function to the common module: natural_sort

--- bbcflib/common.py
+++ bbcflib/common.py
@@ -1,3 +1,4 @@
+###############################################################################
 def normalize_url(url):
     """Produce a fixed form for an HTTP URL.
 
@@ -16,3 +17,19 @@ def normalize_url(url):
     if url.endswith("/"):
         url = url[:-1]
     return url
+
+###############################################################################
+def natural_sort(item):
+    '''
+    Will sort strings that contain numbers correctly
+
+    >>> l = ['v1.3.12', 'v1.3.3', 'v1.2.5', 'v1.2.15', 'v1.2.3', 'v1.2.1']
+    >>> l.sort(key=natural_sort)
+    >>> l.__repr__()
+    "['v1.2.1', 'v1.2.3', 'v1.2.5', 'v1.2.15', 'v1.3.3', 'v1.3.12']"
+    '''
+    import re
+    def try_int(s):
+        try: return int(s)
+        except: return s
+    return map(try_int, re.findall(r'(\d+|\D+)', item))
