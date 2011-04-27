73b91f86e51cde0eedcd59fc29efa520f172d497
Lucas Sinclair <lucas.sinclair@me.com>
2011-04-27 11:30:21 +0200

    Added three new formats for the track subpackage.

---
+++ bbcflib/track/bed.py
@@ -0,0 +1,12 @@
+"""
+============================
+Submodule: bbcflib.track.bed
+============================
+
+Implementation of the BED format.
+"""
+
+#-----------------------------------------#
+# This code was written by Lucas Sinclair #
+# lucas.sinclair@epfl.ch                  #
+#-----------------------------------------#
---
+++ bbcflib/track/wig.py
@@ -0,0 +1,12 @@
+"""
+============================
+Submodule: bbcflib.track.bed
+============================
+
+Implementation of the WIG format.
+"""
+
+#-----------------------------------------#
+# This code was written by Lucas Sinclair #
+# lucas.sinclair@epfl.ch                  #
+#-----------------------------------------#
