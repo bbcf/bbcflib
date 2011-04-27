d374d0c10c525e4a99574cb9acf504a5b588e9b6
Lucas Sinclair <lucas.sinclair@me.com>
2011-04-27 11:27:49 +0200

    Version is now a string

--- bbcflib/__init__.py
+++ bbcflib/__init__.py
@@ -78,7 +78,7 @@ of this license is in the LICENSE.txt file.
 
 b'This module needs Python 2.6 or later.'
 
-__version__ = 1.1.0
+__version__ = '1.1.0'
 
 from ConfigParser import ConfigParser
 from genrep import GenRep, Assembly
