c73d13c0643265b6ed11f3b346cdc5d70f6de981
Lucas Sinclair <lucas.sinclair@me.com>
2011-05-02 10:55:24 +0200

    Starting BED test suite

---
+++ bbcflib/track/test_bed.py
@@ -0,0 +1,24 @@
+# Genreral Modules #
+import os
+
+# Specific Modules #
+from ..track import Track, new
+from ..common import named_temporary_path
+
+# Specific Variables #
+from .test_variables import track_collections
+
+# Unittesting module #
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+###################################################################################
+class Test_Read(unittest.TestCase):
+    def runTest(self):
+        t = track_collections['Validation']['1']
+        with Track(t['orig_path']) as t['track']:
+            # Just the first feature #
+            data = t['track'].read()
+            self.assertEqual(data.next(), ('chr1', 0, 10, 'Validation feature 1', 10.0))
