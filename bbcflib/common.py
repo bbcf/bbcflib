c9e62ba4655d57ac5fc0bb54299bc8e0be9cf805
Lucas Sinclair <lucas.sinclair@me.com>
2011-05-02 15:58:46 +0200

    Changing the memoize class for it to work on methods and not functions

--- bbcflib/common.py
+++ bbcflib/common.py
@@ -47,15 +47,42 @@ def named_temporary_path(suffix=''):
     return path
 
 ###############################################################################
-class memoize_once(object):
+class memoized_method(object):
     '''Decorator that caches a function's return value the first time
     it is called. If called later, the cached value is returned, and
-    not re-evaluated'''
-    def __init__ (self, f):
-        self.f = f
-        self.mem = []
-    def __call__ (self, *args, **kwargs):
-        if len(self.mem) == 0:
-            self.mem.append(self.f(*args, **kwargs))
-        return self.mem[0]
+    not re-evaluated
+    
+    This class is meant to be used as a decorator of methods. The return value
+    from a given method invocation will be cached on the instance whose method
+    was invoked. All arguments passed to a method decorated with memoize must
+    be hashable.
+    
+    If a memoized method is invoked directly on its class the result will not
+    be cached. Instead the method will be invoked like a static method:
+    class Obj(object):
+        @memoized_method
+        def add_to(self, arg):
+            return self + arg
+    Obj.add_to(1)    # not enough arguments
+    Obj.add_to(1, 2) # returns 3, result is not cached
+    '''
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, objtype=None):
+        from functools import partial
+        if obj is None:
+            return self.func
+        return partial(self, obj)
+    def __call__(self, *args, **kw):
+        obj = args[0]
+        try:
+            cache = obj.__cache
+        except AttributeError:
+            cache = obj.__cache = {}
+        key = (self.func, args[1:], frozenset(kw.items()))
+        try:
+            res = cache[key]
+        except KeyError:
+            res = cache[key] = self.func(*args, **kw)
+        return res
 
