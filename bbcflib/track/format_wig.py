10c557e4d382afea7c05ccf64f9677592e7daccd
Lucas Sinclair <lucas.sinclair@me.com>
2011-05-31 14:10:27 +0200

    Modified the random wig track generator

--- bbcflib/track/format_wig.py
+++ bbcflib/track/format_wig.py
@@ -118,38 +118,38 @@ class GenomicFormat(TextTrack, ProxyTrack):
 
 
 ###########################################################################
-def random_track(kind='fixed', number_of_features=32, size=100, range=1000, jump=10000, small_jump=200, orig_start=0, chrs=16):
+def random_track(kind='fixed', chrs=16, number_of_regions=32, orig_start=0, length=100, score=1000, jump=50000):
     import random
     chr = 0
     if kind == 'fixed':
         yield 'track type=wiggle_0 name="Pol2 Signal" description="Chip-Seq" source="Random generator"\n'
-        for i in xrange(number_of_features):
-            if i % (number_of_features / chrs) == 0:
+        for i in xrange(number_of_regions):
+            if i % (number_of_regions / chrs) == 0:
                 chr += 1
                 end  = orig_start
             start = end   + (random.randint(0,jump))
-            end   = start + (random.randint(1,size))
-            multiplier = random.randint(1,range) 
-            sys.stdout.write('fixedStep chrom=chr' + str(chr) + ' start=' + str(start) + ' step=1' + '\n')
-            for x in xrange(start, end):
-                sys.stdout.write(str(multiplier + multiplier * random.random()) + '\n')
-            for x in xrange(start, end):
-                sys.stdout.write('0\n')
-            start = end   + small_jump + (random.randint(0,jump))
-            end   = start +              (random.randint(1,size))
-            multiplier = random.randint(1,range) 
-            for x in xrange(start,end):
+            end   = start + (random.randint(1,length))
+            multiplier = random.randint(1,score) 
+            yield 'fixedStep chrom=chr' + str(chr) + ' start=' + str(start) + ' step=1' + '\n'
+            for x in xrange((end - start)):
                 yield str(multiplier + multiplier * random.random()) + '\n'
+            for x in xrange((end - start)/2):
+                yield '0\n'
+            end   = start + (random.randint(1,length))
+            multiplier = random.randint(1,score) 
+            constant = str(multiplier + multiplier * random.random()) 
+            for x in xrange((end - start)):
+                yield constant + '\n'
     if kind == 'variable':
         yield 'track type=wiggle_0 name="Rap1 Peaks" description="Chip-Seq" source="Random generator"\n'
-        for i in xrange(number_of_features*2):
-            if i % ((number_of_features*2) / chrs) == 0:
+        for i in xrange(number_of_regions*2):
+            if i % ((number_of_regions*2) / chrs) == 0:
                 chr += 1
                 end  = orig_start
-                sys.stdout.write('variableStep chrom=chr' + str(chr) + '\n')
+                yield 'variableStep chrom=chr' + str(chr) + '\n'
             start = end   + (random.randint(0,jump))
-            end   = start + (random.randint(1,int(size/2)))
-            multiplier = random.randint(1,range) 
+            end   = start + (random.randint(1,int(length/2)))
+            multiplier = random.randint(1,score) 
             for x in xrange(start,end):
                 yield str(x) + ' ' + str(multiplier + multiplier * random.random()) + '\n'
 
