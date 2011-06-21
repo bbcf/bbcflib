06678317e3d039268da039bc5a9bf2f941ec4a2a
Lucas Sinclair <lucas.sinclair@me.com>
2011-06-21 18:14:35 +0200

    Moved the 'get_scores_frequencies' to a new file created for the purpose of containing such special methods that use functions specific to a certain type of format

---
+++ bbcflib/track/format_sql_extras.py
@@ -0,0 +1,19 @@
+"""
+===================================
+Submodule: bbcflib.track.format_sql
+===================================
+
+Implementation of special methods of the Track object specific
+to the SQL format and not part of the standard functionality
+that all formats should have.
+"""
+
+class SQLExtras(object):
+    def get_scores_frequencies(self):
+        scores = {}
+        for chrom in self:
+            self.cursor.execute("SELECT count (*), score FROM '" + chrom + "' GROUP BY score")
+            for result in self.cursor:
+                if result[1] not in scores: scores[result[1]] =  result[0]
+                else:                       scores[result[1]] += result[0]
+        return scores
