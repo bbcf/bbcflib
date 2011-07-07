0c8167e4895054fce04863f8dad2630306c8a280
Lucas Sinclair <lucas.sinclair@me.com>
2011-07-07 14:39:46 +0200

    Py-magic wasn't working at all for me. I couldn't find the version 5 anywhere. This is an attempt at fixing the situation and isolating the magic functionality in a subpackage. (bis)

---
+++ bbcflib/track/magic/__init__.py
@@ -0,0 +1,42 @@
+"""
+===============================
+Subpackage: bbcflib.track.magic
+===============================
+
+This function uses the py-magic project at github.com/ahupp/python-magic to guess formats, but since it needs a special version and that isn't in PyPI we can't really rely on it.
+
+"""
+
+# Built-in modules #
+from pkg_resources import resource_filename
+
+###############################################################################
+def guess_file_format(path):
+    # Link between descriptions and file extension #
+    known_formats = {
+        'SQLite 3.x database':                          'sql',
+        'SQLite database (Version 3)':                  'sql',
+        'Hierarchical Data Format (version 5) data':    'hdf5',
+        'track line definition with type BED document': 'bed',
+        'track line definition with type PSL document': 'psl',
+        'track line definition with type GFF document': 'gff',
+        'track line definition with type GTF document': 'gtf',
+        'track line definition with type WIG document': 'wig',
+        'MAF document':                                 'maf',
+    }
+    # Try import #
+    try: import magic
+    except ImportError: return ''
+    # Try version #
+    if not hasattr(magic, 'open'): return ''
+    # Add our definitions #
+    mime = magic.open(magic.NONE)
+    mime.load(file=resource_filename(__name__, 'magic_data'))
+    filetype = mime.file(path)
+    # Otherwise try standard definitions #
+    if not filetype in known_formats:
+        mime = magic.open(magic.NONE)
+        mime.load()
+        filetype = mime.file(path)
+    # Try the conversion dict #
+    return known_formats.get(filetype, '')
---
+++ bbcflib/track/magic/test_magic.py
@@ -0,0 +1,19 @@
+# Internal modules #
+from ..track_collection import track_collections
+from .. import magic
+
+# Unittesting module #
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# Nosetest flag #
+__test__ = True
+
+###################################################################################
+class Test_BED(unittest.TestCase):
+    def runTest(self):
+        path = track_collections['Validation'][1]['path']
+        ftype = magic.guess_file_format(path)
+        self.assertEqual(ftype, 'bed')
