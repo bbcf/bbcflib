21b508e315d00cb6f8a230fe3f518dbc23fcbf27
Jonathan MERCIER <bioinfornatics@gmail.com>
2011-07-11 07:56:21 +0200

    start fix test

--- tests/bbcflib.py
+++ tests/bbcflib.py
@@ -2,7 +2,7 @@ import unittest
 
 def suites():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite("bbcflib.email")
+    suite.addTest(unittest.makeSuite("bbcflib.email"))
     return suite
 
 if __name__ == "__main__":
