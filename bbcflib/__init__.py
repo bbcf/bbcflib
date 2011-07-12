539ca3a0ddd8a1f13606a8369e17c3128857e9f4
Lucas Sinclair <lucas.sinclair@me.com>
2011-07-12 16:59:30 +0200

    Added 'with-doctest' as a default option to nose

--- bbcflib/__init__.py
+++ bbcflib/__init__.py
@@ -67,7 +67,7 @@ where X.Y is the version of Python you run it with.
 
 To run the test suite, in the distribution directory, run::
 
-    $ nosetests --with-doctest
+    $ nosetests
 
 =======
 License
