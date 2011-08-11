c536f5776df19bfdbeb587e3c4cf5d236203db81
Fabrice David <fab.dav@gmail.com>
2011-08-11 13:21:29 +0200

    debug genrep get_sequence

--- bbcflib/genrep.py
+++ bbcflib/genrep.py
@@ -126,7 +126,7 @@ class GenRep(object):
         
         for e in input_list:
             h_data.get(e[0],[])
-            h_data(e[0]).append([e[1],e[2]])
+            h_data[e[0]].append([e[1],e[2]])
         for chr in h_data.keys():
             h_data[chr].sort()
             res_list=get_sequence(chr, h_data[chr])
