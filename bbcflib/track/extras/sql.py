3fe72a0ab865363c5ee38bd6f5d87c9304f5f9cb
Jacques Rougemont <jacques.rougemont@epfl.ch>
2011-08-23 15:12:27 +0200

    crazy sql to get binned cumuls (untested), inline re-shuffling

--- bbcflib/track/extras/sql.py
+++ bbcflib/track/extras/sql.py
@@ -15,34 +15,58 @@ import random
 from ... import track
 
 ###################################################################################
+#### cumulative binned counts:
+#   select y.s,sum(x.c) from (select round(score) as s, count(*) as c from chr1 group by s) as x, (select round(score) as s, count(*) as c from chr1 group by s) as y where x.s>y.s group by y.s order by y.s;
 class TrackExtras(object):
+    def get_cumul_binned_scores(self, roundoff=1):
+        '''Computes cumulative counts for binned scores. 'roundoff' gives the accuracy of the bins, namely
+        score*roundoff is an integer
+        '''
+        counts = {}
+        subqry = "(SELECT ROUND(score*%i)/%i AS s, count(*) as c from '%s' group by s)"
+        for chrom in self:
+            self.cursor.execute("SELECT y.s,sum(x.c) FROM "+subqry %(roundoff,roundoff,chrom)+" AS x, "+subqry %(roundoff,roundoff,chrom)+" AS y WHERE x.s>y.s GROUP BY y.s")
+            temp = dict(tuple(x) for x in self.cursor)
+            if not(len(temp)): continue
+            kmax = max(temp.keys())
+            if not(kmax in counts): counts[kmax] = 0
+            ckeys = counts.keys()
+            k2 = ckeys.pop(0)
+            for k in temp.keys():
+                while k2<k:
+                    k2 = ckeys.pop(0)
+                counts[k] = counts[k2]+temp[k]
+        return counts
+
     def get_scores_frequencies(self):
         '''Example output:
         scores = {100.0: 1, 10.0: 3, 1.0: 2}
         '''
         scores = {}
         for chrom in self:
-            self.cursor.execute("SELECT count (*), score FROM '" + chrom + "' GROUP BY score")
+            self.cursor.execute("SELECT count(*), ROUND(score) AS s FROM '" + chrom + "' GROUP BY s")
             for result in self.cursor:
                 if result[1] not in scores: scores[result[1]] =  result[0]
                 else:                       scores[result[1]] += result[0]
         return scores
 
-    def shuffle_track(self, new_path, repeat_number=1):
-        ''' Makes a new track with the same number of features as the
-        original track or multiplied with a factor.'''
+    def read_shuffled(self, chrom, repeat_number=1, **kwargs):
+        ''' Yields randomly located features of the same length as the original track.'''
+        chr_len = self.chrmeta[chrom]['length']
+        for i in range(repeat_number):
+            for feat in self.read(chrom, **kwargs):
+                feat_len = feat[1]-feat[0]
+                feature = list(feat)
+                feature[0] = random.randint(0, chr_len-feat_len)
+                feature[1] = feature[0]+feat_len
+                yield tuple(feature)
+
+    def shuffle_track(self, new_path, repeat_number=1, **kwargs):
+        ''' Makes a new track with 'repeat_number' features for every feature of the
+        original track.'''
         with track.new(new_path, "sql", name="Random track") as t:
-            # Copy meta data #
             t.chrmeta    = self.chrmeta
             t.attributes = self.attributes
-            # Define generator #
-            def shuffle_features(length, data):
-                for i, feature in enumerate(data):
-                    distance     = feature[1] - feature[0]
-                    random_start = random.randint(0, length - distance)
-                    random_end   = random_start + distance
-                    yield (random_start, random_end, 0.0, "random_feature_"+str(i), 0,"Random feature" ) # start, end, score, name, strand, sttributes, ...
-            # Iterate #
             for chrom in self:
-                for i in range(repeat_number):
-                    t.write(chrom, shuffle_features(self.chrmeta[chrom]['length'], self.read(chrom)))
+                t.write( chrom, self.read_shuffled( chrom, repeat_number, **kwargs ) )
+
