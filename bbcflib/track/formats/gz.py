022db758a525b2f50ff5a76bf5546e28919285d6
Lucas Sinclair <lucas.sinclair@me.com>
2011-08-24 15:21:13 +0200

    Preliminary gzip support tests

--- bbcflib/track/formats/gz.py
+++ bbcflib/track/formats/gz.py
@@ -7,19 +7,26 @@ Implementation of a compression layer GNU zip format.
 """
 
 # Built-in modules #
-import gzip
+import os, gzip
 
 # Internal modules #
-from bbcflib.track.track_util import determine_format, import_implementation
+from bbcflib.track.track_util import import_implementation
 
 ################################################################################
 def TrackFormat(path, format=None, name=None, chrmeta=None, datatype=None, readonly=False, empty=False):
-    if not format: format = determine_format(path)
+    # TODO: recognize the format of a compressed file if no extension in the path
+    format = os.path.splitext(os.path.splitext(path)[0])[1][1:]
     implementation = import_implementation(format)
     base = implementation.TrackFormat
     #----------------------------------#
     class TrackCompressed(base):
-        @property
-        def file_obj(self): return gzip.open(self.path, 'r')
+        def open(self, path, mode):
+            return gzip.open(path, mode)
     #----------------------------------#
     return TrackCompressed(path, format, name, chrmeta, datatype, readonly, empty)
+
+#-----------------------------------#
+# This code was written by the BBCF #
+# http://bbcf.epfl.ch/              #
+# webmaster.bbcf@epfl.ch            #
+#-----------------------------------#
---
+++ bbcflib/track/tests/gz.py
@@ -0,0 +1,34 @@
+# Built-in modules #
+import os, shutil
+
+# Internal modules #
+from bbcflib import track
+from bbcflib.track.common import named_temporary_path
+from bbcflib.track.track_collection import track_collections, yeast_chr_file
+
+# Unittesting module #
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# Nosetest flag #
+__test__ = True
+
+################################################################################
+class Test_Read(unittest.TestCase):
+    def runTest(self):
+        path = track_collections['Compressed'][1]['path']
+        with track.load(path) as t:
+            # Just the first feature #
+            data = t['track'].read()
+            self.assertEqual(data.next(), ('chr1', 0, 10, 'Validation feature 1', 10.0, 0))
+            # Number of features #
+            data = t['track'].read()
+            self.assertEqual(len(list(data)), 12)
+
+#-----------------------------------#
+# This code was written by the BBCF #
+# http://bbcf.epfl.ch/              #
+# webmaster.bbcf@epfl.ch            #
+#-----------------------------------#
