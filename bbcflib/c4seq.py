210bd0dedfbaebb6cd80b43444bfc625fba7651c
Fabrice David <fab.dav@gmail.com>
2011-08-31 15:16:30 +0200

    debug sortedBedGraph

--- bbcflib/c4seq.py
+++ bbcflib/c4seq.py
@@ -137,7 +137,7 @@ def density_to_countsPerFrag(ex,density_file,density_name,assembly_name,reffile,
 	sortedBedGraph=common.cat([headerFile,resBedGraph])
 	ex.add(sortedBedGraph,description="bedgraph:res_segToFrag_"+density_name+" (bedGraph sorted)")	
 	sortedBedGraph_sql=unique_filename_in()
-	with track.load("sortedBedGraph",'bedGraph') as t:
+	with track.load(sortedBedGraph,'bedGraph') as t:
                 t.convert(sortedBedGraph_sql,'sql')
 	ex.add(sortedBedGraph_sql,description="sql:res_segToFrag_"+density_name+" (bedGraph sorted)")
 	return [wd+outdir+".sql",countsPerFragFile,res,resBedGraph,sortedBedGraph,sortedBedGraph_sql]
