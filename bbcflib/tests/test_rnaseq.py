7dd6ffab6929d43e49bca7ff6d5d86838e096f59
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-09-25 15:38:19 +0200

    actually it's better for testing to have an invertible (square) matrix.

--- bbcflib/tests/test_rnaseq.py
+++ bbcflib/tests/test_rnaseq.py
@@ -14,33 +14,34 @@ __test__ = True
 class Test_Rnaseq(unittest2.TestCase):
     def setUp(self):
 	# add some that are not in the mappings from Ensembl
-	self.counts = [[12,12,12,12,12,12],[3,3,3,3,3,3]]
-	self.dexons = dict(zip(["e1","e2","e3","e4","e5","e6","e"],zip(*self.counts)))
+	self.counts = numpy.array([[12,12,12,12,12,12],[3,3,3,3,3,3]], dtype=numpy.float32)
+	self.dexons = dict(zip(["e1","e2","e3","e4","e5","e"],zip(*self.counts)))
 	self.gene_ids = ["g1","g2"]
 	self.exon_mapping = {
 	    "e1":(["t1","t2","t3","t5"],"g1"), "e2":(["t1","t2","t3","t5"],"g1"),
 	    "e3":(["t2","t3","t4","t5"],"g1"), "e4":(["t3","t4","t5"],"g1"),
-	    "e5":(["t4","t5"],"g1"), "e6":(["t4","t5"],"g1"), "e":(["t"],"g2")}
+	    "e5":(["t4","t5"],"g1"), "e":(["t"],"g2")}
 	self.transcript_mapping = {
 	    "t1":"g1", "t2":"g1", "t3":"g1", "t4":"g1","t5":"g1", "t":"g2"}
 	self.trans_in_gene = {
 	    "g1":["t1","t2","t3","t4","t5"], "g2":["t"]}
 	self.exons_in_trans = {
 	    "t1":["e1","e2"], "t2":["e1","e2","e3"], 
-	    "t3":["e1","e2","e3","e4"], "t4":["e3","e4","e5","e6"], 
-            "t5":["e1","e2","e3","e4","e5","e6"],"t":["e"]}
+	    "t3":["e1","e2","e3","e4"], "t4":["e3","e4","e5"], 
+            "t5":["e1","e2","e3","e4","e5"], "t":["e"]}
 	"""
-	|==================g1===================| |===g2===|
-	|.e1.| |.e2.| |.e3.| |.e4.| |.e5.| |.e6.| |...e...|
+	|==================g1============| |===g2===|
+	|.e1.| |.e2.| |.e3.| |.e4.| |.e5.| |...e...|
 	|-----t1----|
 	|---------t2-------|
 	|------------t3-----------|
-	              |------------t4-----------| 
-        |-------------------t5------------------| |----t---|
+	              |-------t4---------| 
+        |--------------t5----------------| |----t---|
 	"""
     def test_transcripts_expression(self):
 	texp = transcripts_expression(self.gene_ids, self.transcript_mapping,
 		self.trans_in_gene, self.exons_in_trans, self.dexons)
+	self.assertListEqual(list(texp["t"]), [12,3])
     def test_genes_expression(self):
 	gexp = genes_expression(self.gene_ids, self.exon_mapping, self.dexons)
 	self.assertListEqual(list(gexp["g1"]), [5*12,5*3])
