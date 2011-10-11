4554b844de84fead663ca28182635d5a4976185e
Marion <marion.leleu@epfl.ch>
2011-10-11 13:11:59 +0200

    add demultiplex.py

--- bbcflib/createlib.py
+++ bbcflib/createlib.py
@@ -13,6 +13,9 @@ import sys, getopt, os, json, re
 import tarfile
 
 def load_libraryParamsFile(paramsfile):
+	'''
+		Return a dictionary with the parameters required for the creation of a new library
+	'''
 	paramslib={}
 	with open(paramsfile) as f:
 		for s in f.readlines():
@@ -39,7 +42,11 @@ def load_libraryParamsFile(paramsfile):
 
  
 @program
-def call_getRestEnzymeOccAndSeq(assembly_or_fasta,prim_site,sec_site,l_seg,g_rep, remote_working_directory, l_type='typeI'):
+def call_getRestEnzymeOccAndSeq(assembly_or_fasta,prim_site,sec_site,l_seg,g_rep, remote_working_directory, l_type='typeI',script_path):
+	'''
+		Will create segments and fragments files of the new library from the genome sequence (via a call to getRestEnzymeOccAndSeq.pl)
+		The genome sequence (assembly_or_fasta) can be given either as a genrep assembly or as a fasta file.  
+	'''
 	print(assembly_or_fasta)
 	print(isinstance(assembly_or_fasta,genrep.Assembly))
 	if isinstance(assembly_or_fasta,genrep.Assembly):
@@ -64,17 +71,20 @@ def call_getRestEnzymeOccAndSeq(assembly_or_fasta,prim_site,sec_site,l_seg,g_rep
 	logFile = unique_filename_in()
 	outfiles=[segFile, fragFile, logFile,fasta_file]
 	print("outfiles will be:"+outfiles[0])
-	script_dir='/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_SAM/'
+#	script_path='/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_SAM/'
 	if cmp(l_type,'typeI') == 0:
 		print('type:typeI')
-		return {'arguments': [script_dir+"getRestEnzymeOccAndSeq.pl","-i",fasta_file,"-m",prim_site,"-s",sec_site,"-l",l_seg,"-o",segFile,"-f",fragFile,"-x",logFile],
+		return {'arguments': [script_path+"getRestEnzymeOccAndSeq.pl","-i",fasta_file,"-m",prim_site,"-s",sec_site,"-l",l_seg,"-o",segFile,"-f",fragFile,"-x",logFile],
                         'return_value':outfiles}
 	else:
-                return {'arguments': [script_dir+"getRestEnzymeOccAndSeq_forNele.pl","-i",fasta_file,"-m",prim_site,"-s",sec_site,"-l",l_seg,"-o",segFile,"-f",fragFile,"-x",logFile],
+                return {'arguments': [script_path+"getRestEnzymeOccAndSeq_typeII.pl","-i",fasta_file,"-m",prim_site,"-s",sec_site,"-l",l_seg,"-o",segFile,"-f",fragFile,"-x",logFile],
                         'return_value':outfiles}
 
 
 def parse_fragFile(fragfile):
+	'''
+		Parse fragment file to create segment info bed file and fragment bed file	
+	'''
 	segInfoBedFile=unique_filename_in()
 	fragmentBedFile=unique_filename_in()
 	segmentBedFile=unique_filename_in()
@@ -108,6 +118,10 @@ def call_coverageBed(file1,file2):
 	return {"arguments": ['coverageBed','-a',file1,'-b',file2], "return_value":None}
 
 def getCoverageInRepeats(ex,infile,genomeName='mm9',via='lsf'):
+	'''
+	Will complete the segment info bed file with the coverage in repeats of each segment 	
+	For now, works only for mm9, hg19 and dm3 	
+	'''
 	repeatsPath="/archive/epfl/bbcf/data/genomes/repeats/"
 	repeatsFile=repeatsPath+'/'+genomeName+'/'+genomeName+'_rmsk.bed'
 	print("repeatsFile="+repeatsFile)
@@ -126,6 +140,9 @@ def getCoverageInRepeats(ex,infile,genomeName='mm9',via='lsf'):
 	return resfile
 
 def getEnzymeSeq(enzyme_id,enzymes_dict=None):
+	'''
+		Return the restriction site corresponding to a given enzyme id (from existing enzymes)
+	'''
 	if enzymes_dict == None or not isinstance(enzymes_dict,list):
 		enzymes='/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/enzymes.json'
 	        g=open(enzymes)
@@ -138,6 +155,9 @@ def getEnzymeSeq(enzyme_id,enzymes_dict=None):
 	return None
 
 def getEnzymeId(enzyme_seq,enzymes_dict=None):
+	'''
+		Return the enzyme id corresponding to a given restriction site sequence (from existing enzymes).
+	'''
 	if enzymes_dict == None or not isinstance(enzymes_dict,list):
 		enzymes='/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/enzymes.json'
 		g=open(enzymes)
@@ -149,6 +169,9 @@ def getEnzymeId(enzyme_seq,enzymes_dict=None):
 	return 0
 	
 def lib_exists(params,libs_dict=None,returnType="id"):
+	'''
+		Return id or filename corresponding to the library described in params. 	
+	'''
 	if libs_dict == None :	
 		libs='/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/libraries.json'
 		f=open(libs)
@@ -193,14 +216,17 @@ def call_gunzip(path):
 	return {"arguments": call, "return_value": output}
 
 # *** main call to create the library
-def createLibrary(ex,fasta_allchr,params, g_rep):	
+def createLibrary(ex,fasta_allchr,params, g_rep,script_path):
+	'''
+		main call to create the library
+	'''	
 	if len(params['primary'])<2 or len(params['secondary'])<2:
 		print('Some parameters are missing, cannot create the library')
 		print('primary='+params['primary']+" ; "+'secondary='+params['secondary'])
 		return [None,None,None,None]
 	print('Will call call_getRestEnzymeOccAndSeq')
 	print(fasta_allchr)
-	libfiles=call_getRestEnzymeOccAndSeq(ex,fasta_allchr,params['primary'],params['secondary'],params['length'],g_rep, ex.remote_working_directory + "/", params['type'])
+	libfiles=call_getRestEnzymeOccAndSeq(ex,fasta_allchr,params['primary'],params['secondary'],params['length'],g_rep, ex.remote_working_directory + "/", params['type'],script_path)
 	print('parse fragment file to create segment info bed file and fragment bed file\n')
 	bedfiles=parse_fragFile(libfiles[1])
 	print('calculate coverage in repeats for segments')
@@ -209,7 +235,7 @@ def createLibrary(ex,fasta_allchr,params, g_rep):
 	return([libfiles,bedfiles,resfile,infos_lib])
 
 
-def get_libForGrp(ex,group,fasta_or_assembly,new_libraries, job_id, g_rep):
+def get_libForGrp(ex,group,fasta_or_assembly,new_libraries, job_id, g_rep,script_path):
 	#wd_archive="/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/" #temporary: will be /scratch/cluster/monthly/htsstation/4cseq/job.id
 	lib_dir = "/scratch/cluster/monthly/htsstation/4cseq/" + str(job_id) + "/"
 	print "Group:\n"
@@ -222,7 +248,7 @@ def get_libForGrp(ex,group,fasta_or_assembly,new_libraries, job_id, g_rep):
 		print("lib_id="+str(lib_id))
 		if lib_id == 0 and ex_libfile == None :
 			print("will call createlib.createLibrary with:"+str(fasta_or_assembly)+" and "+ library_filename)
-			libfiles=createLibrary(ex,fasta_or_assembly,paramslib, g_rep);
+			libfiles=createLibrary(ex,fasta_or_assembly,paramslib, g_rep,script_path);
 			reffile=libfiles[2]
 			ex.add(reffile,description='bed:new_library_grp')
 			new_libraries.append({'library':libfiles[3]})	
---
+++ bbcflib/demultiplex.py
@@ -0,0 +1,251 @@
+from bein import *
+from bein.util import *
+import sys, getopt, os
+
+# call 
+#python demultiplex.py -i "/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/export_part.fa" -p "/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/primers.fa" -x 30 -n 1 -s 108 -l 50
+
+@program
+def exportToFasta(exportFile,n=1,x=22):
+	faFile=unique_filename_in()
+	scriptPath="/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/"
+	return{'arguments': ["python",scriptPath+"exportToFasta.py","-i",exportFile,"-n",str(n),"-x",str(x)],
+               'return_value':faFile}
+
+@program
+def fastqToFasta(fqFile,n=1,x=22):
+	faFile=unique_filename_in()
+	scriptPath="/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/"
+        return{'arguments': ["python",scriptPath+"fastqToFasta.py","-i",fqFile,"-o",faFile,"-n",str(n),"-x",str(x)],
+               'return_value':faFile}
+
+def split_exonerate(filename,n=1,x=22,l=30):	    
+	correction={}
+	files={}
+	filenames={}
+	with open(filename,"r") as f:
+		for s in f:
+			s=s.strip('\n')
+			if re.search(r'vulgar',s):
+				key = s.split(' ')[5].split('|')[0]
+				if not key in correction:
+					correction[key]=len(s.split('|')[1])-len(s.split('|')[3])+1
+					filenames[key]=unique_filename_in()					
+					files[key]=open(filenames[key],"w")
+				k=int(s.split(' ')[3])-int(s.split(' ')[7])+correction[key]
+	#			print("key="+str(key)+";correction="+str(correction[key])+";k="+str(k))
+				seq=s.split(' ')[1].split('_')[1][k:l+k]
+				qual=s.split(' ')[1].split('_')[2][k:l+k]
+				files[key].write("@"+s.split(' ')[1].split('_')[0]+"_"+seq+"_"+qual+"\n"+seq+"\n+\n"+qual+"\n")
+	for f in files.itervalues():
+		f.close()
+	return filenames
+
+
+
+@program
+def raw_exonerate(fastaFile,dbFile,minScore=77,n=1,x=22,l=30):
+	return{'arguments': ["exonerate","--model","affine:local","-o","-4","-e","-12","-s",str(minScore),fastaFile,dbFile],
+	       'return_value': None} 
+
+def exonerate(ex,infile, dbFile, minScore=77,n=1,x=22,l=30,via="local"):
+#	filename = unique_filename_in()
+	#kwargs["stdout"] = filename
+        print "Will prepare fasta file from input file\n"
+	subfiles=split_file(ex,infile,n_lines=8000000)
+        if re.search(r'\.fa$',infile) or re.search(r'\.fasta$',infile):
+                faSubFiles=subfiles
+        elif re.search(r'export',infile):
+                futures=[exportToFasta.nonblocking(ex,exportFile=sf,n=n,x=x,via=via) for sf in subfiles]
+		faSubFiles=[f.wait() for f in futures]
+        elif re.search(r'\.fastq$',infile) or re.search(r'\.fq$',infile):
+		futures=[fastqToFasta.nonblocking(ex,sf,n=n,x=x,via=via) for sf in subfiles]
+                faSubFiles=[f.wait() for f in futures]
+
+	for f in faSubFiles:
+		ex.add(f,description="input.fasta (part)")
+	
+	print("Will call raw_exonerate for each fasta files")	
+#	faSubFiles=split_file(ex,faFile,n_lines=500000)
+	futures = []
+	res = []
+	resExonerate = []
+	for sf in faSubFiles:
+		subResFile = unique_filename_in() 
+		futures.append(raw_exonerate.nonblocking(ex,sf,dbFile,minScore=minScore,n=n,x=x,l=l,via=via,stdout=subResFile))
+		resExonerate.append(subResFile)
+	for f in futures: f.wait()
+	for f in resExonerate:
+		ex.add(f,description="exonerate (part)")
+		res.append(split_exonerate(f,n=n,x=x,l=l))
+
+	print res
+ 
+	return res
+
+
+def demultiplex(ex,fastaFile,dbFile,minScore=77,n=1,x=22,l=30,via="local"):
+	resFiles={}
+	print("in demultiplex, call exonerate\n")
+	exonerated = exonerate(ex, fastaFile, dbFile, minScore=int(minScore),n=n,x=x,l=int(l),via=via)
+#	ex.add(exonerated,description="fileExonerate")
+#	split_files = split_exonerate(exonerated,n=n, x=x, l=l)
+	print("demultiplex done\n")
+	for sr in exonerated:
+		for k,v in sr.iteritems():
+               		print("k="+k)
+                        if k in resFiles:
+				f=open(resFiles[k],"a+")
+				f.write(open(v).read())
+				f.close()
+			else:
+                        	resFiles[k]=v
+	return resFiles
+
+def workflow_groups(ex, job, scriptPath):
+	processed = {}
+#	job_groups=job.groups
+#	job_groups=['group','grp2']
+#	for gid, group in job_groups.iteritems():
+#		for rid,run in group['runs'].iteritems():
+	for i in range(1,1):
+		for j in range(1,1):	
+			group=job['group']
+			rid=gid['run']
+#demultiplex(ex,infile,opts['-p'],int(opts['-s']),opts['-n'],opts['-x'],opts['-l'],via="lsf")
+			resExonerate = demultiplex(ex,rid['infile'],group['primersFile'],group['paramsFile'],via='lsf')
+	# !! STILL NEED TO PARSE THE paramsFile
+			ex.add(rid['infile'],description="infile")
+	        	filteredFastq={}
+	        	for k,f in resExonerate.iteritems():
+        		        ex.add(f,description="k:"+k+".fastq")
+
+		        print "Will filter the sequences\n"
+		        filteredFastq=filterSeq(ex,resExonerate,group['primersFile'])
+
+		        for k,f in filteredFastq.iteritems():
+		                ex.add(f,description="k:"+k+"_filtered.fastq")
+				if k in resExonerate:
+					resExonerate.append(f)
+
+			reportFile=unique_filename_in()
+			
+			ex.add(reportFile,description="pdf:report_demultiplexing")
+			resExonerate.append(reportFile)
+
+	return resExonerate 
+
+
+@program
+def call_createReport(numbersFile,reportFile):
+	Rout=unique_filename_in()
+	return{'arguments': ['R','CMD','BATCH','--vanilla','--no-restore','"--args',numbersFile, numbersFile ,reportFile,'"',scriptPath+"plotGraphsDemultiplexing.R",Rout],
+        	'return_value':None}
+
+
+def createReport(ex)
+	countsFile=unique_filename_in()
+	out=open(countsFile,'w')
+	out.write('Primer\tTotal_number_of_reads\tnFiltered\tnValidReads\n')
+	allfiles = common.get_files( ex.id, M )
+	allcounts={}
+	for f in allfiles['fastq'].iteritems()
+		curPrimer=f[0:f.index(':')]
+		if curPrimer not in allcounts: allcounts[curPrimer]=[0,0,0]
+		if re.search(r'filtered',f):
+			allcounts[curPrimer][1]=count_lines(f)
+		else:
+			allcounts[curPrimer][0]=count_lines(f)
+	
+	resFile=unique_filename_in()
+	call_createReport(countsFile,resFile)
+#bsub -J "${runName}_generateReportDemultiplexing" -o "${wd}${runName}_generatReportDemultiplexing.log" -e "${wd}${runName}_generateReportDemultiplexing.err" "${RPath}R CMD BATCH --vanilla --no-restore '--args ${reportStep1} ${reportStep2} ${plotsDemultiplexing}' ${scriptsDirectory}plotGraphsDemultiplexing.R ${wd}${runName}_generateReportDemultiplexing.Rout"
+	
+
+def filterSeq(ex,fastqFiles,primersFile):
+#seqToFilter=`awk -v primer=${curPrimer} '{if($0 ~ /^>/){n=split($0,a,"|");curPrimer=a[1];gsub(">","",curPrimer); if(curPrimer == primer){seqToFilter="";for(i=5;i<n;i++){if(a[i] !~ /Exclude=/){seqToFilter=seqToFilter""a[i]","}} if(a[n] !~ /Exclude=/){seqToFilter=seqToFilter""a[n]}else{gsub(/,$/,"",seqToFilter)};print seqToFilter}}}' ${primersFile}`
+	seqToFilter={}
+	filenames={}
+	with open(primersFile,"r") as f:
+		for s in f:	
+			if re.search(r'^>',s):
+				key=s.split('|')[0].replace(">","")
+				filenames[key]=unique_filename_in() 
+				seqToFilter[key]=open(filenames[key],"w")
+				for i in range(4,len(s.split('|'))):
+					if not cmp(s.split('|')[i],'.') == 0 or not cmp(s.split('|')[i],'Exclude'):
+						seqToFilter[key].write(">seq"+str(i)+"\n"+s.split('|')[i]+"\n")
+	
+	print "Will build bowtie indexes for seqToFilter\n" 
+	indexSeqToFilter={}
+	for k,f in seqToFilter.iteritems():
+		f.close()
+	
+	indexFiles={}	
+	for k,f in filenames.iteritems():
+                ex.add(f,description=k+"_seqToFilter.fa")
+		#indexFiles[k]=add_bowtie_index(ex,f,description=k+'_bowtie index',stdout="../out")
+		indexFiles[k]=bowtie_build.nonblocking(ex,f,via='lsf')
+
+	print "Wait for index\n"
+	bowtie_index={}
+	unalignedFiles={}
+	alignedFiles={}
+	futures={}
+
+	for k,f in filenames.iteritems():		
+		#bowtie_index[k]=indexFiles[k]
+		bowtie_index[k]=indexFiles[k].wait()
+		unalignedFiles[k]=unique_filename_in()
+		print("primer="+k+"=>index="+bowtie_index[k])
+		print "Will filter reads (call bowtie)\n"
+		bwtarg=["-a","-q","-n","2","-l","20","--un",unalignedFiles[k]]
+		futures[k]=bowtie.nonblocking( ex, bowtie_index[k], fastqFiles[k],  
+                                     bwtarg, via='lsf')
+	
+	for k,f in filenames.iteritems():
+		alignedFiles[k]=futures[k].wait()
+
+	return unalignedFiles
+
+
+"""
+opts = dict(getopt.getopt(sys.argv[1:],"i:p:x:n:s:l:",[])[0])
+M = MiniLIMS("/scratch/cluster/daily/pipeline3Cseq/data")
+
+working_dir="/scratch/cluster/daily/pipeline3Cseq/"
+os.chdir(working_dir)
+
+# !!! Filter undigested seq...
+with execution(M,remote_working_directory=working_dir) as ex:
+	print("inputFile="+opts['-i']+"n="+opts['-n']+";x="+opts['-x']+";primersFile="+opts['-p'])
+	#infile="/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/AB4C_export_part.txt"
+#	infile="/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/AB4C_part.fastq"
+	infile=opts['-i']
+	
+	print "Will prepare fasta file from input file\n"
+	if re.search(r'\.fa$',infile) or re.search(r'\.fasta$',infile):
+		faFile=infile
+	elif re.search(r'export',infile):
+		faFile=exportToFasta(ex,infile,int(opts['-n']),int(opts['-x']))
+	elif re.search(r'\.fastq$',infile) or re.search(r'\.fq$',infile):
+		faFile=fastqToFasta(ex,infile,int(opts['-n']),int(opts['-x']))
+	ex.add(faFile,description="input.fasta")
+
+	print "Will start the demultiplexing process\n"
+        #res=demultiplex(ex,"/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/export_part.fa","/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/primers.fa",108,1,30,50)
+        #res=demultiplex(ex,"/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/export_part.fa","/archive/epfl/bbcf/mleleu/pipeline_vMarion/pipeline_3Cseq/vWebServer_Bein/tests/primers.fa",int(opts['-s']),int(opts['-n']),int(opts['-x']),int(opts['-l']))
+        res=demultiplex(ex,faFile,opts['-p'],int(opts['-s']),int(opts['-n']),int(opts['-x']),int(opts['-l']))
+#	ex.add(res)
+	filteredFastq={}
+	for k,f in res.iteritems():
+		ex.add(f,description=k+"_sol2sanger.fastq")
+	
+	print "Will filter the sequences\n"
+	filteredFastq=filterSeq(ex,res,opts['-p'])
+
+	for k,f in filteredFastq.iteritems():
+                ex.add(f,description=k+"_filtered.fastq")
+
+"""
+
