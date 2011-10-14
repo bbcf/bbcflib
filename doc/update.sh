d03549fcfd42dd7ef73bdbe2ff9ad03f040162ac
Jacques Rougemont <jacques.rougemont@epfl.ch>
2011-10-14 13:32:34 +0200

    update the other way...

--- doc/update.sh
+++ doc/update.sh
@@ -1,4 +1,2 @@
 make clean html
-sudo chown -R apache:apache _build/html
-rsync -av --owner --group _build/html/ root@ginger.epfl.ch:/srv/bbcflib
-sudo chown -R YOURUSERNAME:YOURGROUP _build/html
+ssh -t ginger.epfl.ch "cd /srv/bbcflib; sudo scp -r '"$USER"@"$HOST":"$PWD"/_build/html/*' ."
