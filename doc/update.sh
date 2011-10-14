1f56ab80b71b7765435d1c370d8d434fcf7a46c6
Jacques Rougemont <jacques.rougemont@epfl.ch>
2011-10-14 13:55:15 +0200

    simpler way

--- doc/update.sh
+++ doc/update.sh
@@ -1,2 +1,2 @@
 make clean html
-ssh -t ginger.epfl.ch "cd /srv/bbcflib; sudo scp -r '"$USER"@"$HOST":"$PWD"/_build/html/*' ."
+/usr/bin/ssh -t ginger.epfl.ch "sudo /usr/bin/scp -r '"$USER"@"$HOST":"$PWD"/_build/html/*' /srv/bbcflib"
