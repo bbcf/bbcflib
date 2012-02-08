75bdd544aa33586470b5c764fba3a0825b83d174
Jacques Rougemont <jacques.rougemont@epfl.ch>
2012-02-08 21:58:49 +0100

    was not a good idea

--- bbcflib/mapseq.py
+++ bbcflib/mapseq.py
@@ -250,7 +250,7 @@ def get_fastq_files( ex, job, dafl=None, set_seed_length=True):
 # BAM/SAM files
 ###############
 @program
-def sam_to_bam(sam_filename, bam_filename=unique_filename_in()):
+def sam_to_bam(sam_filename, bam_filename=None):
     """Convert *sam_filename* to a BAM file.
 
     *sam_filename* must obviously be the filename of a SAM file.
@@ -258,6 +258,7 @@ def sam_to_bam(sam_filename, bam_filename=unique_filename_in()):
 
     Equivalent: ``samtools view -b -S -o ...``
     """
+    if bam_filename is None: bam_filename = unique_filename_in()
     return {"arguments": ["samtools","view","-b","-S","-o",bam_filename,sam_filename],
             "return_value": bam_filename}
 
@@ -283,24 +284,26 @@ def replace_bam_header(header, bamfile):
             'return_value': bamfile}
 
 @program
-def sort_bam(bamfile, filename=unique_filename_in()):
+def sort_bam(bamfile, filename=None):
     """Sort a BAM file *bamfile* by chromosome coordinates.
 
     Returns the filename of the newly created, sorted BAM file.
 
     Equivalent: ``samtools sort ...``
     """
+    if filename is None: filename = unique_filename_in()
     return {'arguments': ['samtools','sort',bamfile,filename],
             'return_value': filename + '.bam'}
 
 @program
-def sort_bam_by_read(bamfile, filename=unique_filename_in()):
+def sort_bam_by_read(bamfile, filename=None):
     """Sort a BAM file *bamfile* by read names.
 
     Returns the filename of the newly created, sorted BAM file.
 
     Equivalent: ``samtools sort -n ...``
     """
+    if filename is None: filename = unique_filename_in()
     return {'arguments': ['samtools','sort','-n',bamfile,filename],
             'return_value': filename + '.bam'}
 
