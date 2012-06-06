a937716c08c69f959d161848a4529f4a35c270be
Jacques Rougemont <jacques.rougemont@epfl.ch>
2012-06-06 16:54:31 +0200

    minus one line

--- bbcflib/btrack/sql.py
+++ bbcflib/btrack/sql.py
@@ -216,11 +216,11 @@ class SqlTrack(Track):
 
 ################################ Write ##########################################
     def _clip(self):
-        for chrom in self.chrmeta:
-            chrsize = self.chrmeta[chrom]['length']
+        for chrom,val in self.chrmeta.iteritems():
+            chrsize = 
             sql_command = "UPDATE '%s' SET start=0 WHERE start<0" %(chrom)
             self.cursor.execute(sql_command)
-            sql_command = "UPDATE '%s' SET end=%i WHERE end>%i" %(chrom,chrsize,chrsize)
+            sql_command = "UPDATE '%s' SET end=%i WHERE end>%i" %(chrom,val['length'],val['length'])
             self.cursor.execute(sql_command)
             sql_command = "DELETE FROM '%s' WHERE end<=start" %(chrom)
             self.cursor.execute(sql_command)
