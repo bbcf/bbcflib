838faab83d320c7441cae2b536b3d396f35fed90
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-09-14 14:09:21 +0200

    basis of the program

--- bbcflib/junctions.py
+++ bbcflib/junctions.py
@@ -40,7 +40,7 @@ def soapsplice(unmapped_R1, unmapped_R2, index, output=None, **options):
     #"SOAPsplice-v1.9/bin/soapsplice -d $index -1 $path1$r1 -2 $path2$r2 -I $insertsize -o $outpath$out -f 2 "
 
 
-def junctions_workflow(ex, job, bam_files, via="lsf"):
+def junctions_workflow(ex, job, bam_files, ref_genome, index, via="lsf"):
     """
     Main function of the workflow.
 
@@ -50,114 +50,28 @@ def junctions_workflow(ex, job, bam_files, via="lsf"):
     :param bam_files: a complicated dictionary such as returned by mapseq.get_bam_wig_files.
     :param via: 'local' or 'lsf'.
     """
-    group_names={}; group_ids={}; conditions=[]
     assembly = genrep.Assembly(assembly=job.assembly_id,intype=2)
-    groups = job.groups
-    for gid,group in groups.iteritems():
+    ref_genome = untar_genome_fasta(assembly, ref_genome, convert=True) # cf snp.py
+
+    group_names={}
+    group_ids={}
+    for gid,group in job.groups.iteritems():
         gname = str(group['name'])
         group_names[gid] = gname
         group_ids[gname] = gid
 
-    """ Define conditions """
-    for gid,files in bam_files.iteritems():
-        k = 0
-        for rid,f in files.iteritems():
-            k+=1
-            cond = group_names[gid]+'.'+str(k)
-            conditions.append(cond)
-    ncond = len(conditions)
-
-    #Exon labels: ('exonID|geneID|start|end|strand|type', length)
-    exons = fetch_labels(bam_files[groups.keys()[0]][groups.values()[0]['runs'].keys()[0]]['bam'])
-
-    """ Extract information from bam headers """
-    exonsID=[]; genesID=[]; genesName=[]; starts=[]; ends=[]; strands=[]
-    exon_lengths={}; exon_to_gene={}; badexons=[]
-    for e in exons:
-        length = e[1]
-        E = e[0].split('|')
-        exon = E[0]; gene = E[1]; start = int(E[2]); end = int(E[3]); strand = int(E[4])
-        if end-start>1:
-            starts.append(start)
-            ends.append(end)
-            strands.append(strand)
-            exon_lengths[exon] = float(length)
-            exonsID.append(exon)
-            genesID.append(gene)
-            exon_to_gene[exon] = gene
-        else: badexons.append(e)
-    [exons.remove(e) for e in badexons]
-
-    print "Load mappings"
-    """ [0] gene_mapping is a dict ``{gene ID: (gene name,start,end,length,strand,chromosome)}``
-        [1] transcript_mapping is a dictionary ``{transcript ID: (gene ID,start,end,length,strand,chromosome)}``
-        [2] exon_mapping is a dictionary ``{exon ID: ([transcript IDs],gene ID,start,end,strand,chromosome)}``
-        [3] trans_in_gene is a dict ``{gene ID: [IDs of the transcripts it contains]}``
-        [4] exons_in_trans is a dict ``{transcript ID: [IDs of the exons it contains]}`` """
-    (gene_mapping, transcript_mapping, exon_mapping, trans_in_gene, exons_in_trans) = fetch_mappings(assembly)
-
-    """ Map remaining reads to transcriptome """
-    additionals = {}; unmapped_bam = {}; unmapped_fastq = {}
-    refseq_path = assembly.index_path
+    unmapped_fastq = {}
     for gid, group in job.groups.iteritems():
-        k = 0
         for rid, run in group['runs'].iteritems():
-            k +=1
-            cond = group_names[gid]+'.'+str(k)
-            unmapped_fastq[cond] = bam_files[gid][rid].get('unmapped_fastq')
-            if unmapped_fastq[cond] and os.path.exists(refseq_path+".1.ebwt"):
-                unmapped_bam[cond] = mapseq.map_reads(ex, unmapped_fastq[cond], {}, refseq_path, \
-                      remove_pcr_duplicates=False, bwt_args=[], via=via)['bam']
-                if unmapped_bam[cond]:
-                    sam = pysam.Samfile(unmapped_bam[cond])
-                else: sam = []
-                additional = {}
-                for read in sam:
-                    t_id = sam.getrname(read.tid).split('|')[0]
-                    if transcript_mapping.get(t_id) and exons_in_trans.get(t_id):
-                        lag = 0
-                        r_start = read.pos
-                        r_end = r_start + read.rlen
-                        E = exons_in_trans[t_id]
-                        for e in E:
-                            e_start, e_end = exon_mapping[e][2:4]
-                            e_len = e_end-e_start
-                            if lag <= r_start <= lag+e_len:
-                                additional[e] = additional.get(e,0) + 0.5
-                            if lag <= r_end <= lag+e_len:
-                                additional[e] = additional.get(e,0) + 0.5
-                            lag += e_len
-                additionals[cond] = additional
-                sam.close()
+            # Define pairs of fastq files ...
+            unmapped_fastq[gid] = unmapped_fastq.setdefault(gid,[]).append(bam_files[gid][rid].get('unmapped_fastq'))
+        if unmapped_fastq[gid]:
+            R1 = None
+            R2 = None
+        junc_file = soapsplice(R1,R2,index)
 
-    """ Build exon pileups from bam files """
-    print "Build pileups"
-    exon_pileups={}; nreads={}
-    for gid,files in bam_files.iteritems():
-        k = 0
-        for rid,f in files.iteritems():
-            k+=1
-            cond = group_names[gid]+'.'+str(k)
-            exon_pileup = build_pileup(f['bam'], exons)
-            if unmapped_fastq[cond] and cond in additionals:
-                for a,x in additionals[cond].iteritems():
-                    exon_pileup[a] = exon_pileup.get(a,0) + x
-            exon_pileups[cond] = [exon_pileup[e] for e in exonsID] # {cond1.run1: {pileup}, cond1.run2: {pileup}...}
-            nreads[cond] = nreads.get(cond,0) + sum(exon_pileup.values()) # total number of reads
-            print "....Pileup", cond, "done"
 
-    """ Print counts for exons """
-    hconds = ["counts."+c for c in conditions] + ["rpkm."+c for c in conditions]
-    exons_data = [exonsID]+list(counts)+list(rpkm)+[starts,ends,genesID,genesName,strands,echr]
-    exons_file = None; genes_file = None; trans_file = None
-
-    print "Get scores of exons"
-    exons_data = zip(*exons_data)
-    header = ["ExonID"] + hconds + ["Start","End","GeneID","GeneName","Strand","Chromosome"]
-    exons_data = zip(*exons_data)
-    exons_file = save_results(ex, exons_data, conditions, group_ids, assembly, header=header, feature_type="EXONS")
-
-    return {"exons":exons_file, "genes":genes_file, "transcripts":trans_file}
+    return know_junctions, new_junctions
 
 
 #------------------------------------------------------#
