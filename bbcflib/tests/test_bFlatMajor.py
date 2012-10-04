7e339e6eb5eeec3ced96ab7f162c60e2ee6a33a6
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-10-04 10:43:43 +0200

    some tests

--- bbcflib/tests/test_bFlatMajor.py
+++ bbcflib/tests/test_bFlatMajor.py
@@ -360,11 +360,17 @@ class Test_Scores(unittest.TestCase):
         expected = [(10,15,6.),(30,40,6.)]
         self.assertListEqual(res,expected)
 
-        #features = fstream([(5,40,'gene1'),(20,50,'gene2')], fields=['start','end','name'])
-        #scores = fstream([(10,20,6.),(30,40,6.)], fields=['start','end','score'])
-        #res = list(filter_scores(scores,features))
-        #expected = [(10,15,6.),(30,40,6.)]
-        #self.assertListEqual(res,expected)
+        features = fstream([(5,40,'gene1'),(20,50,'gene2')], fields=['start','end','name'])
+        scores = fstream([(10,20,6.),(30,40,6.)], fields=['start','end','score'])
+        res = list(filter_scores(scores,features))
+        expected = [(10,20,6.),(30,40,6.),(30,40,6.)] # get it twice
+        self.assertListEqual(res,expected)
+
+        features = fstream([(0,30,'gene1'),(20,50,'gene2')], fields=['start','end','name'])
+        scores = fstream([(10,40,6.)], fields=['start','end','score'])
+        res = list(filter_scores(scores,features))
+        expected = [(10,30,6.),(20,40,6.)] # get overlapping regions
+        self.assertListEqual(res,expected)
 
     def test_score_by_feature(self):
         features = fstream([(5,15,'gene1'),(30,40,'gene2')], fields=['start','end','name'])
