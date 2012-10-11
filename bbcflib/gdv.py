8e90802e738eb956d7021655570b9ccf575872f3
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-10-11 13:42:11 +0200

    gdv doc

--- bbcflib/gdv.py
+++ bbcflib/gdv.py
@@ -17,13 +17,14 @@ default_url = 'http://gdv.epfl.ch/pygdv'
 ################################################################################
 def get_project(mail, key, project_key, serv_url=default_url):
     '''
-    Get a project by it's key.
-    :param mail : login in TEQUILA
-    :param key : an user-specific key (ask it to GDV admin)
-    :param project_key : the project key
+    Get a project by its key.
+
+    :param mail : login to TEQUILA.
+    :param key : an user-specific key (ask for it to a GDV admin).
+    :param project_key : the project key.
     '''
     query_url = '%s/%s' % (normalize_url(serv_url), 'projects/get')
-    request = {'mail':mail, 
+    request = {'mail':mail,
                'key':key,
                'project_key':project_key
                }
@@ -45,14 +46,15 @@ def delete_track(mail, key, track_id, serv_url=default_url):
 def new_project(mail, key, name, assembly_id, serv_url=default_url):
     '''
     Create a new project on GDV.
-    :param mail : login in TEQUILA
-    :param key : an user-specific key (ask it to GDV admin)
-    :param name : name of the project
-    :param assembly_id : the assembly identifier in GenRep (must be BBCF_VALID)
-    :return a JSON
+
+    :param mail: login to TEQUILA.
+    :param key: a user-specific key (ask for it to a GDV admin).
+    :param name: name of the project.
+    :param assembly_id: the assembly identifier in GenRep (must be BBCF_VALID).
+    :rtype: JSON
     '''
     query_url = '%s/%s' % (normalize_url(serv_url), 'projects/create')
-    request = {'mail':mail, 
+    request = {'mail':mail,
                'key':key,
                'name':name,
                'assembly':assembly_id
@@ -61,23 +63,25 @@ def new_project(mail, key, name, assembly_id, serv_url=default_url):
 
 
 def single_track(mail, key, serv_url=default_url,
-                 assembly_id=None, project_id=None, 
-                 url=None, fsys=None, trackname=None, extension=None, 
+                 assembly_id=None, project_id=None,
+                 url=None, fsys=None, trackname=None, extension=None,
                  force=False, delfile=False):
     '''
     Create a new track on GDV.
-    :param mail : login in TEQUILA
-    :param key : an user-specific key (ask it to GDV admin)
-    :param name : name of the project
-    :param assembly_id : the assembly identifier in GenRep (must be BBCF_VALID). Could be optional if a project_id is specified.
-    :param project_id : the project identifier to add the track to.
-    :param url : an url pointing to a file.
-    :param extension : extension of the file provided.
-    :param fsys : if the file is on the same file system, a filesystem path
-    :param trackname : the name to give to the track 
-    :param force : A boolean. Force the file to be recomputed.
-    :param delfile : If true and file comming from fsys, the original file will be removed after job success
-    :return a JSON
+
+    :param mail: login to TEQUILA.
+    :param key: an user-specific key (ask for it to a GDV admin).
+    :param name: name of the project.
+    :param assembly_id: the assembly identifier in GenRep (must be BBCF_VALID).
+        Optional if a project_id is specified.
+    :param project_id: the project identifier to add the track to.
+    :param url: an url pointing to a file.
+    :param extension: extension of the file provided.
+    :param fsys: if the file is on the same file system, a filesystem path.
+    :param trackname: the name to give to the track.
+    :param force: A boolean. Force the file to be recomputed.
+    :param delfile: If true and file coming from fsys, the original file will be removed after job success.
+    :rtype: JSON
     '''
     query_url = '%s/%s' % (normalize_url(serv_url), 'tracks/create')
     request = {'mail' : mail, 'key' : key}
@@ -93,19 +97,21 @@ def single_track(mail, key, serv_url=default_url,
 
 
 def multiple_tracks(mail, key, serv_url=default_url,
-                    assembly_id=None, project_id=None, 
-                    urls=[], fsys_list=[], tracknames=[], extensions=[], 
+                    assembly_id=None, project_id=None,
+                    urls=[], fsys_list=[], tracknames=[], extensions=[],
                     force=False):
     '''
-    Create tracks on GDV
-    :param extensions : a list of extensions, separated by whitespace.
-    :param urls : a list of urls separated by whitespaces. 
-    :param fsys : if the file is on the same file system, a filesystem path
-    :param fsys_list :  a list of fsys separated by whitespaces.
-    :param tracknames : a list of file name, in the same order than the files uploaded.
-    If there is differents parameters given, the first file uploaded will be file_upload, 
-    then urls, url, fsys and finally fsys_list. The list is separated by whitespaces.
-    For other params :see single_track
+    Create tracks in GDV.
+
+    :param extensions: a list of extensions, separated by whitespace.
+    :param urls: a list of urls separated by whitespaces.
+    :param fsys: if the file is on the same file system, a filesystem path
+    :param fsys_list:  a list of fsys separated by whitespaces.
+    :param tracknames: a list of file names, in the same order than the files uploaded.
+        If there is differents parameters given, the first file uploaded will be file_upload,
+        then urls, url, fsys and finally fsys_list. The list is separated by whitespaces.
+
+    For other params: see single_track
     '''
     tracks = []
     if isinstance(tracknames,basestring): tracknames = tracknames.split()
@@ -115,19 +121,19 @@ def multiple_tracks(mail, key, serv_url=default_url,
     ntracks = len(urls)+len(fsys_list)
     tr = tracknames+[None]*(ntracks-len(tracknames))
     ex = extensions+[None]*(ntracks-len(extensions))
-    tracks = [single_track(mail, key, serv_url=serv_url, 
-                           assembly_id=assembly_id, project_id=project_id, 
-                           url=u, trackname=tr[n], extension=ex[n], force=force) 
+    tracks = [single_track(mail, key, serv_url=serv_url,
+                           assembly_id=assembly_id, project_id=project_id,
+                           url=u, trackname=tr[n], extension=ex[n], force=force)
               for n,u in enumerate(urls)] \
-              + [single_track(mail, key, serv_url=serv_url, 
-                              assembly_id=assembly_id, project_id=project_id, 
-                              fsys=f, trackname=tr[len(urls)+n], extension=ex[len(urls)+n], force=force) 
+              + [single_track(mail, key, serv_url=serv_url,
+                              assembly_id=assembly_id, project_id=project_id,
+                              fsys=f, trackname=tr[len(urls)+n], extension=ex[len(urls)+n], force=force)
                  for n,f in enumerate(fsys_list)]
     return tracks
 
 def send_it(url, request, return_type='json'):
     '''
-    Send the request to GDV and return the result. As JSON or as a request.read().
+    Send the request to GDV and return the result, as JSON or as a `request.read()`.
     '''
     req = urllib2.urlopen(url, urllib.urlencode(request))
     if return_type == 'json': return json.load(req)
