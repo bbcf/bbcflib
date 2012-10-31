e02695611cb65eb127e0998b842cae289d4d5b07
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-10-31 14:18:35 +0100

    added junctions stuff; moved diff_analysis call from run_rnaseq.py to rnaseq.py.

--- bbcflib/rnaseq.py
+++ bbcflib/rnaseq.py
@@ -15,12 +15,13 @@ Note that the resulting counts on transcripts are approximate.
 """
 
 # Built-in modules #
-import os, pysam, math
+import os, pysam, math, itertools
 
 # Internal modules #
-from bbcflib.common import writecols, set_file_descr, unique_filename_in
+from bbcflib.common import writecols, set_file_descr, unique_filename_in, cat
 from bbcflib import mapseq, genrep
-from bbcflib.bFlatMajor.common import cobble, sorted_stream
+from bbcflib.mapseq import add_and_index_bam, sam_to_bam
+from bbcflib.bFlatMajor.common import cobble, sorted_stream, map_chromosomes, duplicate, apply
 from bbcflib.btrack import track, FeatureStream, convert
 from bein import program
 
@@ -378,7 +379,8 @@ def to_rpkm(counts, lengths, nreads):
 
 
 #@timer
-def rnaseq_workflow(ex, job, bam_files, pileup_level=["exons","genes","transcripts"], via="lsf"):
+def rnaseq_workflow(ex, job, bam_files, pileup_level=["exons","genes","transcripts"], via="lsf",
+                    rpath=None, junctions=None):
     """
     Main function of the workflow.
 
@@ -466,6 +468,12 @@ def rnaseq_workflow(ex, job, bam_files, pileup_level=["exons","genes","transcrip
                 additionals[cond] = additional
                 sam.close()
 
+    """ Find splice junctions """
+    if junctions:
+        soapsplice_index = assembly.soapsplice_index_path
+        #soapsplice_options = # check if Sanger or Illumina format
+        find_junctions(ex,job,bam_files,soapsplice_index)
+
     """ Build exon pileups from bam files """
     print "Build pileups"
     exon_pileups={}; nreads={}
@@ -527,7 +535,10 @@ def rnaseq_workflow(ex, job, bam_files, pileup_level=["exons","genes","transcrip
         trans_data = [transID]+list(zip(*tcounts))+list(zip(*trpkm))+[tstart,tend,genesID,genesName,tstr,tchr]
         trans_file = save_results(ex, trans_data, conditions, group_ids, assembly, header=header, feature_type="TRANSCRIPTS")
 
-    return {"exons":exons_file, "genes":genes_file, "transcripts":trans_file}
+    result = {"exons":exons_file, "genes":genes_file, "transcripts":trans_file}
+    differential_analysis(ex, result, rpath)
+
+    return 0
 
 
 #-------------------------- DIFFERENTIAL ANALYSIS ----------------------------#
@@ -607,6 +618,106 @@ def differential_analysis(ex, result, rpath, design=None, contrast=None):
                     o = clean_deseq_output(o)
                     ex.add(o, description=desc)
             except Exception as exc: print "Skipped differential analysis: %s \n" % exc
+
+
+#-------------------------- SPLICE JUNCTIONS SEARCH ----------------------------#
+
+@program
+def soapsplice(unmapped_R1, unmapped_R2, index, output=None, path_to_soapsplice=None, options={}):
+    """Binds 'soapsplice'. Returns a text file containing the list of junctions.
+
+    :param unmapped_R1: (str) path to the fastq file containing the 'left' reads.
+    :param unmapped_R2: (str) path to the fastq file containing the 'right' reads.
+    :param index: (str) path to the SOAPsplice index.
+    :param output: (str) output file name.
+    :param path_to_soapsplice: (str) path to the SOAPsplice executable.
+        If not specified, the program must be in your $PATH.
+    :param options: (dict) SOAPsplice options, given as {opt: value}.
+    :rtype: str
+
+    Main options::
+
+        -p: number of threads, <= 20. [1]
+        -S: 1: forward strand, 2: reverse strand, 3: both. [3]
+        -m: maximum mismatch for one-segment alignment, <= 5. [3]
+        -g: maximum indel for one-segment alignment, <= 2. [2]
+        -i: length of tail that can be ignored in one-segment alignment. [7]
+        -t: longest gap between two segments in two-segment alignment. [500000]
+        -a: shortest length of a segment in two-segment alignment. [8]
+        -q: input quality type in FASTQ file (0: Illumina, 1: Sanger). [0]
+        -L: maximum distance between paired-end reads. [500000]
+        -l: minimum distance between paired-end reads. [50]
+        -I: insert length of paired-end reads.
+    """
+    if not output: output = unique_filename_in()
+    path_to_soapsplice = path_to_soapsplice or 'soapsplice'
+    args = [path_to_soapsplice,'-d',index,'-1',unmapped_R1,'-2',unmapped_R2,'-o',output,'-f','2']
+    opts = [] # -q 1 for Sanger format
+    for k,v in options.iteritems(): opts.extend([str(k),str(v)])
+    return {"arguments": args+opts, "return_value": output}
+
+def find_junctions(ex,job,bam_files,soapsplice_index,path_to_soapsplice=None,soapsplice_options={}):
+    """
+    :param ex: the bein's execution Id.
+    :param job: a Job object (or a dictionary of the same form) as returned from HTSStation's frontend.
+    :param bam_files: a complicated dictionary such as returned by mapseq.get_bam_wig_files.
+    :param index: (str) path to the SOAPsplice index.
+    :param path_to_soapsplice: (str) specify the path to the program if it is not in your $PATH.
+    :param soapsplice_options: (dict) SOAPsplice options, e.g. {'-q',1} .
+    :param via: 'local' or 'lsf'.
+    :rtype: str, str, str
+    """
+    unmapped_fastq = {}
+    for gid, group in job.groups.iteritems():
+        unmapped_fastq[gid] = []
+        for rid, run in group['runs'].iteritems():
+            unmapped = bam_files[gid][rid].get('unmapped_fastq')
+            assert unmapped, "No unmapped reads found."
+            assert isinstance(unmapped,tuple), "Pair-end reads required."
+            unmapped_fastq[gid].append(unmapped)
+        R1 = cat(zip(*unmapped_fastq[gid])[0])
+        R2 = cat(zip(*unmapped_fastq[gid])[1])
+        template = soapsplice(ex,R1,R2,soapsplice_index,path_to_soapsplice=path_to_soapsplice,options=soapsplice_options)
+        junc_file = template+'.junc'
+        sql,bed = convert_junc_file(junc_file,job.assembly)
+        sql_descr = set_file_descr('junctions_%s.sql' % group['name'], \
+                                   group=group['name'],type='sql',step='1',gdv=1)
+        bed_descr = set_file_descr('junctions_%s.bed' % group['name'], \
+                                   group=group['name'],type='bed',step='1',ucsc=1)
+        bam_descr = set_file_descr('junctions_%s.bam' % group['name'], \
+                                   group=group['name'],type='bam',step='1')
+        sam = template+'.sam'
+        bam = sam_to_bam(ex,sam,reheader=job.assembly.name)
+        add_and_index_bam(ex, bam, description=bam_descr)
+        ex.add(sql, description=sql_descr)
+        ex.add(bed, description=bed_descr)
+        ex.add(bam, description=bam_descr)
+
+def convert_junc_file(filename, assembly):
+    """Convert a .junc SOAPsplice output file to sql and bed formats, and return the respective file names.
+
+    :param filename: (str) name of the .junc file to convert.
+    :param assembly: genrep.Assembly object.
+    :rtype: str, str
+    """
+    t = track(filename, format='txt', fields=['chr','start','end','strand','score'], chrmeta=assembly.chrmeta)
+    stream = t.read()
+    # Translate chromosome names
+    s1 = map_chromosomes(stream, assembly.chromosomes)
+    # Add junction IDs
+    s2 = duplicate(s1,'strand','name')
+    C = itertools.count()
+    s3 = apply(s2,'name', lambda x: 'junction'+str(C.next()))
+    # Convert to sql and bed formats
+    outfile = unique_filename_in()
+    sql = outfile + '.sql'
+    bed = outfile + '.bed'
+    out = track(sql, fields=s3.fields, chrmeta=assembly.chrmeta)
+    out.write(s3)
+    convert(sql,bed)
+    return sql, bed
+
+
 
 #------------------------------------------------------#
 # This code was written by Julien Delafontaine         #
