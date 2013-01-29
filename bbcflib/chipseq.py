b4ef6f63112b1af430c3a3578442ebc63d84487c
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-01-29 15:05:49 +0100

    ever more memory

--- bbcflib/chipseq.py
+++ bbcflib/chipseq.py
@@ -108,7 +108,7 @@ def add_macs_results( ex, read_length, genome_size, bamfile,
             m = name['controls'][j]
             nm = (n,m)
             futures[nm] = macs.nonblocking( ex, rl, genome_size, bam, cam,
-                                            args=macs_args, via=via, memory=8 )
+                                            args=macs_args, via=via, memory=12 )
     prefixes = dict((n,f.wait()) for n,f in futures.iteritems())
     for n,p in prefixes.iteritems():
         macs_descr0 = {'step':'macs','type':'none','view':'admin','groupId':n[0][0]}
