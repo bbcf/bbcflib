3915c5127e41707076bad9d4a7ed888a71d40de9
Jacques Rougemont <jacques.rougemont@epfl.ch>
2013-03-13 10:34:33 +0100

    construct an Assembly object from files

--- bbcflib/genrep.py
+++ bbcflib/genrep.py
@@ -27,17 +27,14 @@ equally well be written::
 """
 
 # Built-in modules #
-import urllib2, json, os, re, tarfile
+import urllib2, json, os, re, gzip, tarfile, bz2, sqlite3
 from datetime import datetime
 from operator import itemgetter
 
 # Internal modules #
-from bbcflib.common import normalize_url, unique_filename_in
-from bbcflib import btrack as track
-from bbcflib.bFlatMajor.common import shuffled as track_shuffle
-
-# Other modules #
-import sqlite3
+from bbcflib.common import normalize_url, unique_filename_in, fasta_length
+from bbcflib.btrack import track, ensembl_to_ucsc, FeatureStream
+from bbcflib.bFlatMajor.common import shuffled as track_shuffle, split_field, map_chromosomes
 
 # Constants #
 default_url = 'http://bbcf-serv01.epfl.ch/genrep/'
@@ -45,7 +42,8 @@ default_root = '/db/genrep'
 
 ################################################################################
 class Assembly(object):
-    def __init__(self, assembly=None, genrep=None, intype=0):
+    def __init__(self, assembly=None, genrep=None, intype=0,
+                 fasta=None, annot=None, ex=None, via='local'):
         """
         A representation of a GenRep assembly.
         To get an assembly from the repository, call the Assembly
@@ -105,8 +103,14 @@ class Assembly(object):
         self.genrep = genrep
         self.intype = int(intype)
         self.chromosomes = {}
-        if not(assembly is None):
+        if fasta is not None:
+            self.build_assembly(ex,assembly,fasta,annot,via)
+        elif assembly is not None:
             self.set_assembly(assembly)
+            if annot is not None and os.path.exists(annot):
+                self.annot_path = self.gtf_to_sql(annot)
+        else:
+            raise ValueError("Need an assembly or a fasta file.")
 
     def set_assembly(self, assembly):
         """
@@ -143,6 +147,65 @@ class Assembly(object):
                              if x['chr_name']['assembly_id'] == self.id).next()
             self._add_chromosome(**chrom)
         return None
+
+    def build_assembly(self, ex, assembly, fasta, annot, via):
+        """
+        Build an Assembly object from files.
+        """
+        from bbcflib.mapseq import bowtie_build
+        if assembly is None:
+            if fasta is not None:
+                self.name = splitext( basename( fasta ) )[0]
+            elif annot is not None:
+                self.name = splitext( basename( fasta ) )[0]
+            else:
+                self.name = "_custom_"
+        else:
+            try:
+                assembly = int(assembly)
+                assembly_info = json.load(urllib2.urlopen(urllib2.Request(
+                                """%s/assemblies/%d.json""" % (self.genrep.url, assembly))))
+                chromosomes = json.load(urllib2.urlopen(urllib2.Request(
+                                """%s/chromosomes.json?assembly_id=%d""" %(self.genrep.url, assembly))))
+            except:
+                try:
+                    url = """%s/assemblies.json?assembly_name=%s""" %(self.genrep.url, assembly)
+                    assembly_info = json.load(urllib2.urlopen(urllib2.Request(url)))[0]
+                except IndexError: raise ValueError("URL not found: %s." % url)
+                chromosomes = json.load(urllib2.urlopen(urllib2.Request(
+                            """%s/chromosomes.json?assembly_name=%s""" %(self.genrep.url, assembly))))
+            for c in chromosomes:
+                chrom = dict((str(k),v) for k,v in c['chromosome'].iteritems())
+                cnames = chrom.pop('chr_names')
+                chrom['name'] = (str(x['chr_name']['value']) for x in cnames \
+                                 if x['chr_name']['assembly_id'] == assembly_info['assembly']['id']).next()
+                self._add_chromosome(**chrom)
+            self.name = assembly
+        if fasta is not None:
+            self.fasta_origin = fasta
+            self.fasta_by_chrom = self.untar_genome_fasta()
+            fasta_files = list(set(self.fasta_by_chrom.values()))
+            self.index_path = bowtie_build.nonblocking(ex,fasta_files,
+                                                       via=via,memory=8).wait()
+            chromosomes = {}
+            for f in fasta_files: chromosomes.update(fasta_length(ex,f))
+            for k,chrom in self.chromosomes.iteritems():
+                if chrom['name'] in chromosomes:
+                    chrom_ac = str(k[0])+"_"+str(k[1])+"."+str(k[2]) if k[1] else str(k[0])
+                    if chrom.get('synonyms'):
+                        chromosomes[chrom['name']]['synonyms'] = chrom['synonyms']+","+chrom_ac
+                    else:
+                        chromosomes[chrom['name']]['synonyms'] = chrom_ac
+            self.chromosomes = dict(((k,None,None),v) 
+                                    for k,v in chromosomes.iteritems())
+            if hasattr(self,"annot_origin"):
+                archive = tarfile.open(fasta)
+                input_file = archive.extractfile(self.annot_origin)
+                annot = unique_filename_in()
+                with open(annot,"w") as outf:
+                    [outf.write(line) for line in input_file]
+        if annot is not None and os.path.exists(annot):
+            self.annot_path = self.gtf_to_sql(annot)
 
     def _add_info(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -271,7 +334,7 @@ class Assembly(object):
                 size += cur_chunk
                 slices = _flush_slices(slices,cid[0],chrom['name'],out,pref)
         else:
-            with track.track(regions, chrmeta=self.chrmeta) as t:
+            with track(regions, chrmeta=self.chrmeta) as t:
                 _f = [f for f in ["start","end","name"] if f in t.fields]
                 cur_chunk = 0
                 for cid,chrom in self.chromosomes.iteritems():
@@ -363,9 +426,13 @@ class Assembly(object):
 
     def fasta_path(self, chromosome=None):
         """Return the path to the compressed fasta file, for the whole assembly or for a single chromosome."""
+        if hasattr(self,"fasta_origin"): 
+            if chromosome is not None:
+                return self.fasta_by_chrom[chromosome]
+            return self.fasta_origin
         root = os.path.join(self.genrep.root,"nr_assemblies/fasta")
         path = os.path.join(root,self.md5+".tar.gz")
-        if chromosome != None:
+        if chromosome is not None:
             chr_id = str(chromosome[0])+"_"+str(chromosome[1])+"."+str(chromosome[2])
             root = os.path.join(self.genrep.root,"chromosomes/fasta")
             path = os.path.join(root,chr_id+".fa.gz")
@@ -384,32 +451,115 @@ class Assembly(object):
         :param path_to_ref: (str) path to the fasta file of the reference sequence (possibly .tar).
         :param convert: (bool) True if chromosome names need conversion from id to chromosome name.
         """
+
+        def _rewrite(inf,genomeRef,chrlist):
+            outf = None
+            for line in inf:
+                headpatt = re.search(r'^>(\S+)\s',line)
+                if headpatt: 
+                    chrom = headpatt.groups()[0]
+                    if chrom in chrlist:
+                        line = re.sub(chrom,chrlist[chrom],line)
+                        chrom = chrlist[chrom]
+                    newfa = unique_filename_in()
+                    if outf is not None: outf.close()
+                    outf = open(newfa,"w")
+                    outf.write(line)
+                    genomeRef[chrom] = newfa
+                elif outf is not None: 
+                    outf.write(line)
+            if outf is not None: outf.close()
+
+        if hasattr(self,"fasta_by_chrom") return self.fasta_by_chrom
         if path_to_ref is None: path_to_ref = self.fasta_path()
-        if not(os.path.exists(path_to_ref)): return None
+        if not(os.path.exists(path_to_ref)): 
+            raise ValueError("Reference fasta archive not found: %s."%path_to_ref)
         if convert:
             # Create a dict of the form {'2704_NC_001144.4': chrXII}
             chrlist = dict((v['ac'],k) for k,v in self.chrmeta.iteritems())
         else:
             chrlist = {}
-        archive = tarfile.open(path_to_ref)
         genomeRef = {}
-        for f in archive.getmembers():
-            if f.isdir(): continue
-            inf = archive.extractfile(f)
-            header = inf.readline()
-            headpatt = re.search(r'>(\S+)\s',header)
-            if headpatt: chrom = headpatt.groups()[0]
-            else: continue
-            if convert and chrom in chrlist:
-                header = re.sub(chrom,chrlist[chrom],header)
-                chrom = chrlist[chrom]
-            genomeRef[chrom] = unique_filename_in()
-            with open(genomeRef[chrom],"wb") as outf:
-                outf.write(header)
-                [outf.write(l) for l in inf]
-            inf.close()
-        archive.close()
+        is_gz = path_to_ref.endswith((".gz",".gzip"))
+        is_bz2 = path_to_ref.endswith((".bz2",".bz"))
+        if ".tar" in os.path.basename(path_to_ref) or ".tgz" in os.path.basename(path_to_ref):
+            archive = tarfile.open(path_to_ref)
+            annot_file = None
+            for f in archive.getmembers():
+                if f.isdir(): continue
+                if os.path.splitext(f.name)[0][1:4] in ["sql","gtf"]:
+                    self.annot_origin = f.name
+                    continue
+                input_file = archive.extractfile(f)
+                _rewrite(input_file,genomeRef,chrlist)
+                input_file.close()
+            archive.close()
+        elif is_gz:
+            input_file = gzip.open(path_to_ref,mode='rb')
+            _rewrite(input_file,genomeRef,chrlist)
+            input_file.close()
+        elif is_bz2:
+            input_file = bz2.BZ2File(path_to_ref,mode='r')
+            _rewrite(input_file,genomeRef,chrlist)
+            input_file.close()
+        else:
+            input_file = open(path_to_ref)
+            _rewrite(input_file,genomeRef,chrlist)
+            input_file.close()
         return genomeRef
+
+    def gtf_to_sql(self,gtf_path,sql_path=None):
+        def _split_attributes(stream,field='attributes'):
+            fi = stream.fields.index(field)
+            for x in stream:
+                for y in x[fi].split(';'):
+                    header = re.search(r'\s*(\S+) \S*',y)
+                    if header:
+                        yield header.groups()[0].strip('"')
+
+        def _exon_number(x):
+            if x is None: x='1'
+            return int(x)
+
+        def _fix_exon_id(stream,count):
+            for x in stream:
+                type = x[2]
+                exon_id = x[12]
+                if type == "exon" and not exon_id:
+                    exon_id = "gr_exid_%i"%count
+                    count += 1
+                    x = x[:12]+(exon_id,)+x[13:]
+                yield x
+
+        _intypes = {'exon_number': _exon_number}
+        std_outfields = ['gene_id','gene_name','transcript_id','transcript_name','exon_id','exon_number']
+        sql_params = {'info': {'datatype':'relational'},
+                  'outtypes': {'exon_number': 'integer'},
+                  'chrmeta': self.chrmeta }
+        gtf_read_fields = ['chr','source','name','start','end','strand','frame','attributes']
+        sql_fields = ['chr','biotype','type','start','end','strand','frame']+std_outfields
+        exon_count = 1
+
+        if sql_path is None: sql_path = unique_filename_in()+".sql"
+        if gtf_path is None: 
+            gtf_path = os.path.join(self.genrep.root,"nr_assemblies/gtf/%s_%i.gtf.gz"%(self.md5,0))
+            for n in range(1,100):
+                gtf_path_n = os.path.join(self.genrep.root,"nr_assemblies/gtf/%s_%i.gtf.gz"%(self.md5,n))
+                if not os.path.exists(gtf_path_n): break
+                gtf_path = gtf_path_n
+        gtf = track(gtf_path, intypes=_intypes, chrmeta=self.chrmeta)
+        if gtf.format == 'sql': return gtf_path
+        all_fields = set(_split_attributes(gtf.read(fields=['attributes'])))
+        new_fields = [f for f in all_fields if not(f in std_outfields)]
+        xsplit = split_field(ensembl_to_ucsc(gtf.read(fields=gtf_read_fields)),
+                             outfields=std_outfields+new_fields, infield='attributes', 
+                             header_split=' ', strip_input=True)
+        outf = track(sql_path, fields=sql_fields+new_fields, **sql_params)
+        outf.write(FeatureStream(_fix_exon_id(
+                    map_chromosomes(xsplit,self.chromosomes),exon_count),
+                                 sql_fields[:7]+xsplit.fields[7:]))
+        outf.close()
+        return os.path.abspath(sql_path)
 
     def get_sqlite_url(self):
         """Return the url of the sqlite file containing gene annotations."""
@@ -417,6 +567,8 @@ class Assembly(object):
 
     def sqlite_path(self):
         """Return the path to the sqlite file containing genes annotations."""
+        if hasattr(self,"annot_path"): return self.annot_path
+        if not hasattr(self,"md5"): return None
         root = os.path.join(self.genrep.root,"nr_assemblies/annot_tracks")
         return os.path.join(root,self.md5+".sql")
 
@@ -607,7 +759,7 @@ class Assembly(object):
                 name = "%s|%s" %(gene_id,gene_name)
                 sort_list.append((chr_name,start,end,name,0,int(strand)))
         sort_list.sort()
-        return track.FeatureStream(sort_list,fields=_fields)
+        return FeatureStream(sort_list,fields=_fields)
 
     def annot_track(self,annot_type='gene',chromlist=None,biotype=["protein_coding"]):
         """
@@ -670,7 +822,7 @@ class Assembly(object):
                         sort_list.append((start,end,name)+tuple(v[0][nmax:]))
                 sort_list.sort()
                 for k in sort_list: yield (chrom,)+k[:-1]
-        return track.FeatureStream(_query(),fields=_fields)
+        return FeatureStream(_query(),fields=_fields)
 
     def gene_track(self,chromlist=None,biotype=["protein_coding"]):
         """Return a FeatureStream over all protein coding genes annotation in the genome:
@@ -700,7 +852,7 @@ class Assembly(object):
     @property
     def chrnames(self):
         """Return a list of chromosome names."""
-        namelist = [(v['num'],v['name']) for v in self.chromosomes.values()]
+        namelist = [(v.get('num',v['name']),v['name']) for v in self.chromosomes.values()]
         return [x[1] for x in sorted(namelist)]
 
 ################################################################################
@@ -799,7 +951,6 @@ class GenRep(object):
             pwm = "\n".join([" ".join(["1"]+[str(x[t[y]]) for y in ['A','C','G','T']]) for x in motif['motif']])
             pwmfile.write(pwm)
         return output
-        
         
 
     def assemblies_available(self, assembly=None, filter_valid=True):
