c96a578ef1b2bf1a0152f9cb4e2fcc0706ceaab8
pjacquet <philippe.jacquet@epfl.ch>
2014-05-02 09:34:24 +0200

    Use RColorBrewer in Vplot

--- bbcflib/gfminer/figure/rplots.py
+++ bbcflib/gfminer/figure/rplots.py
@@ -158,9 +158,18 @@ def Vplot(X,Y,output=None,format='pdf',new=True,last=True,**kwargs):
     plotopt,output = _begin(output=output,format=format,new=new,**kwargs)
     robjects.r.assign('xdata',numpy2ri.numpy2ri(X))
     robjects.r.assign('ydata',numpy2ri.numpy2ri(Y))
+    #robjects.r("""
+#library(graphics)
+#smoothScatter(xdata,ydata,colramp=colorRampPalette(c("white","blue","red"))%s)""" %plotopt)
     robjects.r("""
-library(graphics)
-smoothScatter(xdata,ydata,colramp=colorRampPalette(c("white","blue","red"))%s)""" %plotopt)
+    library(RColorBrewer)
+    colramp = colorRampPalette(c("lightgrey","blue","red"),interpolate="spline")
+    trspcol = function(x,alpha="60") {
+        if (is.na(x)) x
+        else paste(x,alpha,sep='')
+    }
+    allcols = sapply(densCols(xdata,ydata,colramp=colramp),trspcol,alpha="10")
+    plot(xdata,ydata,pch='.',col=allcols, cex=4%s)""" %plotopt)
     _end("",last,**kwargs)
     return output
 
